9d6ad286745cba75246f590cff0d7acf6fbeb576
evagolabek <golabek.eva@gmail.com>
2018-09-23 19:17:30 +0200

    reusuable input component

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Button, Card, CardSection, Input } from './common';
 
 //common directory+its index allows to import everything(button, card etc) in one line/more tidy
+// onChangeText event function is called whenever user type text in TextInput
+// setState is called at the same time and its current value is this.state.text
+// plus whenever we call setState the component immediately rerender
+//when it rerender we tell it that the value is this.state.text
+// THE TEXT EXISTS AS A PIECE OF STATE ON OUR COMPONENT
 
 class LoginForm extends Component {
+  state = {text: '' };
   render() {
     return (
       <Card>
-      <CardSection />
-      <CardSection />
+      <CardSection>
+        <Input
+          value={this.state.text}
+          onChangeText={text => this.setState({ text: text })}
+        />
+      </CardSection>
+      <CardSection>
+
+      </CardSection>
 
       <CardSection>
         <Button>
