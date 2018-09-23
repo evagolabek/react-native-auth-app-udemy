cb3d1c212afaaf4661b4d9c5ccc37a3eb0b14602
evagolabek <golabek.eva@gmail.com>
2018-09-23 16:37:35 +0200

    firebase import and setup

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
+// firebase import should be above other imports
+//configuration object(below)is paste from the firebase website
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: "AIzaSyBf94VGTNcjXb_mr2pT8VnD1N5l0Z7MCTQ",
+    authDomain: "authentication-23320.firebaseapp.com",
+    databaseURL: "https://authentication-23320.firebaseio.com",
+    projectId: "authentication-23320",
+    storageBucket: "authentication-23320.appspot.com",
+    messagingSenderId: "469692650204"
+  });
+  }
   render() {
     return (
       <View>
