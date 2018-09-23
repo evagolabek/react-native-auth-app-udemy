2ac2232f9ba4ec7ec98e5c396432ce3ec4203d29
evagolabek <golabek.eva@gmail.com>
2018-09-23 20:29:00 +0200

    password input

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -8,8 +8,10 @@ import { Button, Card, CardSection, Input } from './common';
 //when it rerender we tell it that the value is this.state.text
 // THE TEXT EXISTS AS A PIECE OF STATE ON OUR COMPONENT
 
+//just listing the prop secureTextEntry makes it true 
+
 class LoginForm extends Component {
-  state = {email: '' };
+  state = { email: '', password:'' };
   render() {
     return (
       <Card>
@@ -21,8 +23,15 @@ class LoginForm extends Component {
           onChangeText={email => this.setState({ email })}
         />
       </CardSection>
-      <CardSection>
 
+      <CardSection>
+        <Input
+          secureTextEntry
+          placeholder="password"
+          label="Password"
+          value={this.state.password}
+          onChangeText={password => this.setState({password})}
+        />
       </CardSection>
 
       <CardSection>
